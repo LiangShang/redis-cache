09741d4c93749b27ca7f9ba4634f46ef5922a477
Liang Shang <liang1990.shang@gmail.com>
2016-05-14 22:11:53 +0800

    add attribute in Node.java

--- src/main/java/me/liang/rediscache/lru/Node.java
+++ src/main/java/me/liang/rediscache/lru/Node.java
@@ -1,4 +1,12 @@
 package me.liang.rediscache.lru;
 
+/**
+* This is the lru node that saves the pre, next nodes and the key of the cache
+*/
 public class Node {
+
+    String key;
+    Node pre;
+    Node next;
+
 }
