30a0fd8a85d278120ad507bd98026175d25d2cc1
Liang Shang <liang1990.shang@gmail.com>
2016-05-15 22:36:13 +0800

    add linkedList method for Node

--- src/main/java/me/liang/rediscache/lru/Node.java
+++ src/main/java/me/liang/rediscache/lru/Node.java
@@ -9,4 +9,62 @@ public class Node {
     Node pre;
     Node next;
 
+    public boolean addToNext(Node node) {
+
+        Node nnext = this.next;
+
+        this.next = node;
+        node.next = nnext;
+        node.pre = this;
+        if (nnext != null) {
+            nnext.pre = node;
+        }
+        return true;
+    }
+
+    public boolean addToPre(Node node) {
+
+        Node ppre = this.pre;
+
+        this.pre = node;
+        node.pre = ppre;
+        node.next = this;
+        if (ppre != null) {
+            ppre.next = node;
+        }
+        return true;
+
+    }
+
+    public Node(String key) {
+        this.key = key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public Node setKey(String key) {
+        this.key = key;
+        return this;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public Node setNext(Node next) {
+        this.next = next;
+        return this;
+    }
+
+    public Node getPre() {
+        return pre;
+    }
+
+    public Node setPre(Node pre) {
+        this.pre = pre;
+        return this;
+    }
+
 }
