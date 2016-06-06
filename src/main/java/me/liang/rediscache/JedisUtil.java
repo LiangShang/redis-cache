d2364ea52e8623e32fa85af930cf747db7fed319
Liang Shang <liang1990.shang@gmail.com>
2016-06-06 22:25:42 +0800

    add files and new dependency

---
+++ src/main/java/me/liang/rediscache/JedisUtil.java
@@ -0,0 +1,24 @@
+package me.liang.rediscache;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.Transaction;
+
+import java.util.Objects;
+
+public class JedisUtil {
+
+    // TODO NOT FINISHED
+    public boolean casSet_NOT_FINISHED(JedisPool jedisPool, String key, String former, String changed) {
+
+        Jedis jedis = jedisPool.getResource();
+        jedis.watch(key);
+        if (!Objects.equals(former, jedis.get(key))) return false;
+        Transaction transaction = jedis.multi();
+        transaction.set(key, changed);
+        transaction.exec();
+
+
+        return true;
+    }
+}
